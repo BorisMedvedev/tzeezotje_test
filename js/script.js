a8e3520d6b0a73f300e59e9944147477740725b3
Медведев Борис <bmedved78@mail.ru>
2023-01-29 13:17:47 +0300

    888

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", function (event) {
   jQuery
     .ajax({
       method: "POST",
-      url: 'https://tzeezotje-test.vercel.app/mail.php',
+      url: 'http://index.html/mail.php',
       data: formData,
     })
     .done(function (msg) {
